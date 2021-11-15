d6f3074369f800f0cc9037609f8d9e9c113e990c
Andrzej <andrzej@yexperiment.com>
2021-11-15 01:47:44 +0100

    menu positioning fixes

--- lib/layouts/parts/main_menu.dart
+++ lib/layouts/parts/main_menu.dart
@@ -11,7 +11,7 @@ class MainMenu extends StatelessWidget {
             padding: EdgeInsets.only(top: size.height * 0.015, bottom: size.height * 0.015, right: size.width * 0.02),
             child: Text(text, style: style)
         ) : Padding(
-            padding: EdgeInsets.only(top: size.height * 0.015, bottom: size.height * 0.015, right: size.width * 0.01),
+            padding: EdgeInsets.only(top: size.height * 0.015, bottom: size.height * 0.015, right: size.width * 0.02),
             child: Text("")
         ),
         icon: Padding(
@@ -26,7 +26,8 @@ class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    TextStyle textStyle = Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.bold);
+    TextStyle textStyle = Theme.of(context).textTheme.headline6.copyWith(
+        fontWeight: FontWeight.bold);
     return SingleChildScrollView(
       child: Container(
         padding: EdgeInsets.only(top: size.height * 0.02),
@@ -34,13 +35,13 @@ class MainMenu extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: (size.width < ThemeSizes.tiny)? CrossAxisAlignment.center : CrossAxisAlignment.start,
           children: [
-            menuButton(Icons.home_work_rounded, "Home", textStyle, size),
-            menuButton(Icons.star, "Trends", textStyle, size),
-            menuButton(Icons.notifications_active_rounded, "News", textStyle, size),
-            menuButton(Icons.message_rounded, "Chats", textStyle, size),
-            menuButton(Icons.bookmark_rounded, "Saved", textStyle, size),
-            menuButton(Icons.margin, "Events", textStyle, size),
-            menuButton(Icons.person, "Profile", textStyle, size),
+            menuButton(Icons.home_work_outlined, "Home", textStyle, size),
+            menuButton(Icons.notifications_active_outlined, "News", textStyle, size),
+            menuButton(Icons.star_outline_rounded, "Trends", textStyle, size),
+            menuButton(Icons.bookmark_outline_rounded, "Saved", textStyle, size),
+            menuButton(Icons.calendar_today_outlined, "Events", textStyle, size),
+            menuButton(Icons.person_outline_rounded, "Profile", textStyle, size),
+            menuButton(Icons.message_outlined, "Chats", textStyle, size),
             menuButton(Icons.more_horiz, "More", textStyle, size),
           ],
         )
